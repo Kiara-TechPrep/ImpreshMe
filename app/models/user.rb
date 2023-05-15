086132eb00a35a2cb8d7010beefb0438d93161fc
Kiara Jeff <ki.mij@icloud.com>
2023-05-15 17:31:07 +0000

    imported user, comments, likes & post- addedassos

---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string
+#  password_digest :string
+#  username        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  validates :email, :uniqueness => { :case_sensitive => false }
+  validates :email, :presence => true
+  has_secure_password
+
+  has_many(:posts, { :class_name => "Post", :foreign_key => "user_id", :dependent => :destroy })
+  has_many(:likes, { :class_name => "Like", :foreign_key => "user_id", :dependent => :destroy })
+  has_many(:comments, { :class_name => "Comment", :foreign_key => "user_id", :dependent => :destroy })
+end
